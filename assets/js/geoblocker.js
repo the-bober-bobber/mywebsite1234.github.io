38237115e5111aa435bf172ca6145021a6eacd90
mywebsite1234 <danylo.stoian@gmail.com>
2025-11-18 09:48:30 -0800

    Update geoblocker.js

--- assets/js/geoblocker.js
+++ assets/js/geoblocker.js
@@ -1,4 +1,4 @@
-const validCodes = ["Yqcne2", "Omv46U", "L3v2I0", "fxFl6s", "ZlO3Gy", "t760kv", "006I09", "7cu2ZG", "U9P0Nl", "mjzRK3", "XXXXXX", "dadish3", "bob", "67"];
+const validCodes = ["Yqcne2", "Omv46U", "L3v2I0", "fxFl6s", "ZlO3Gy", "t760kv", "006I09", "7cu2ZG", "U9P0Nl", "mjzRK3", "XXXXXX", "bob", "67"];
 const blank = [""];
 
 if (window.location.pathname.endsWith("blocked.html") && localStorage.getItem("accessGranted") !== "true") {
@@ -64,4 +64,5 @@ function checkInviteCode() {
   }
 
 }
+
 
